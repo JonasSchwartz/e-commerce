0773aab72e217bc8916ed1f24eaeddc7e58fa275
jonas schwartz <92309379+JonasSchwartz@users.noreply.github.com>
2022-06-17 02:41:18 +0200

    first commit

---
+++ backend/data.js
@@ -0,0 +1,116 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: "Nike Slim Shirt",
+            slug: "Nike-Slim-Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg", //679*829
+            price: 120,
+            countInStock: 0,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 16,
+            description:"High Quality Shirt"
+        },
+
+        {
+            _id:"2",
+            name: "Nike fit Shirt",
+            slug: "Nike-fit-Shirt",
+            category: "Shirts",
+            image: "/images/p2.jpg",
+            price:250,
+            countInStock: 1,
+            brand: "Nike",
+            rating: 4.2,
+            numReviews: 24,
+            description:"High Quality Shirt"
+        },
+
+        {
+            _id:"3",
+            name: "Nike slim pants",
+            slug: "Nike-Slim-Pants",
+            category: "Pants",
+            image: "/images/p3.jpg",
+            price:250,
+            countInStock: 20,
+            brand: "Nike",
+            rating: 4.8,
+            numReviews: 110,
+            description:"High Quality Pants"
+        },
+
+        {
+            _id:'4',
+            name: "Nike fit pants",
+            slug: "Nike-fit-Pants",
+            category: "Pants",
+            image: "/images/p4.jpg",
+            price:250,
+            countInStock: 20,
+            brand: "Nike",
+            rating: 4.0,
+            numReviews: 43,
+            description:"High Quality Pants"
+        },
+        {
+            _id:"5",
+            name: "Nike fit shirt",
+            slug: "Nike-Shirt-Blue",
+            category: "Shirts",
+            image: "/images/p5.jpg",
+            price:250,
+            countInStock: 20,
+            brand: "Nike",
+            rating: 4.0,
+            numReviews: 43,
+            description:"High Quality Pants"
+        },
+
+        {
+            _id:"6",
+            name: "Nike Shirt Grey Hoodie",
+            slug: "Nike-Shirt-Grey-Hoodie",
+            category: "Shirts",
+            image: "/images/p6.jpg",
+            price:140,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 4,
+            description:"High Quality Pants"
+        },
+
+        {
+            _id:"7",
+            name: "Nike Jacket Orange ",
+            slug: "Nike-Jacket-Orange",
+            category: "Pants",
+            image: "/images/p7.jpg",
+            price:250,
+            countInStock: 20,
+            brand: "Nike",
+            rating: 3.5,
+            numReviews: 12,
+            description:"High Quality Pants"
+        },
+        {
+            _id:"8",
+            name: "Nike jacket Green",
+            slug: "Nike-Jacket-Green",
+            category: "Jackets",
+            image: "/images/p8.jpg",
+            price:250,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 1,
+            description:"High Quality Pants"
+        },
+
+    ]
+}
+
+export default data
